023f186bf1b91d4eb8eb3630fd8df3300ebbf4dd
gervinoun <gervinpro@gmail.com>
2026-01-21 13:45:55 +0200

    tagurpidi nimi

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,13 @@
 import { ref } from 'vue';
     let message = ref('Sen nii fire');
 
-    function input(event){
-        console.log(event.target.value);
-        message.value = event.target.value;
-    }
 </script>
 
 <template>
     <div class="container">
-    <h1>{{ message }}</h1>
-    <button class="button is-primary" v-on:click="message = 'Hello TA-24A'">Click me!</button>
-    <input class="input" v-bind:value= "message" v-on:input="input">
+    <h1>{{ message.split('').reverse().join('') }}</h1>
+    <button class="button is-primary" @click="message = 'Hello TA-24A'">Click me!</button>
+    <input class="input" v-model = "message">
     </div>
 </template>
 
