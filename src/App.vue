b3fd73999fb05e310eefb241375dd4e2ba92515d
gervinoun <gervinpro@gmail.com>
2026-01-21 13:06:05 +0200

    message input värk

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-    let message = 'Sen nii fire';
+import { ref } from 'vue';
+    let message = ref('Sen nii fire');
+
+    function input(event){
+        console.log(event.target.value);
+        message.value = event.target.value;
+    }
 </script>
 
 <template>
     <div class="container">
     <h1>{{ message }}</h1>
-    <button class="button is-primary">Click me!</button>
+    <button class="button is-primary" v-on:click="message = 'Hello TA-24A'">Click me!</button>
+    <input class="input" v-bind:value= "message" v-on:input="input">
     </div>
 </template>
 
