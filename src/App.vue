b4024793e18c30cc74d1082bb074bad31f74c1fd
gervinoun <gervinpro@gmail.com>
2026-01-21 13:57:10 +0200

    ma rüüpan promethasiini

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <script setup>
-import { ref } from 'vue';
-    let message = ref('Sen nii fire');
+import { ref } from 'vue'
 
+const items = ref(['Sai', 'Piim', 'Viin', 'Õlu'])
+const newItem = ref('')
+
+const addItem = () => {
+  if (newItem.value.trim() !== '') {
+    items.value.push(newItem.value)
+    
+  }
+  newItem.value = ''
+}
 </script>
 
 <template>
-    <div class="container">
-    <h1>{{ message.split('').reverse().join('') }}</h1>
-    <button class="button is-primary" @click="message = 'Hello TA-24A'">Click me!</button>
-    <input class="input" v-model = "message">
+  <div class="container">
+    <div class="field has-addons mt-2">
+      <div class="control is-expanded">
+        <input
+          class="input"
+          type="text"
+          v-model="newItem"
+          @keypress.enter="add"
+        />
+      </div>
+      <div class="control">
+        <button class="button is-info" @click="add">
+          Add Item
+        </button>
+      </div>
     </div>
+
+    <div class="content">
+      <h1>All Items</h1>
+      <ul>
+        <li v-for="(item, index) in items" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
-
-<style>
-
-</style>
