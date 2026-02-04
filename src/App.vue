9ac5e74e50728a3315eabf8dc16d9e4a03a5ac36
gervinoun <gervinpro@gmail.com>
2026-02-04 14:07:55 +0200

    04.02

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <script setup>
-import { ref } from 'vue';
-import Modal from './components/Modal.vue';
-
-
- 
-let modal1Active = ref(false);
- 
+import { ref } from 'vue'
 </script>
- 
+
 <template>
-    <div class="container">
-      <button class="button is-primary" @click="modal1Active=true">Open modal</button>
+  <div class="container">
+    <button
+      class="button is-primary"
+      @click="modal1Active = true"
+    >
+      Open Modal 1
+    </button>
 
+    <button
+      class="button is-info"
+      @click="modal2Active = true"
+    >
+      Open Modal 2
+    </button>
 
-      <Modal :active="modal1Active" @close="modal1Active=false"></Modal>
-    </div>
+    <Modal
+      :active="modal1Active"
+      @close="modal1Active = false"
+    />
+
+    <Modal
+      :active="modal2Active"
+      @close="modal2Active = false"
+    />
+  </div>
 </template>
- 
+
 <style></style>
---
+++ src/components/ImgModal.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Modal from './Modal.vue'
+
+defineProps({
+  active: {
+    type: Boolean,
+    default: false
+  },
+  url: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <Modal :active="active">
+    <p class="image is-4by3">
+      <img :src="url" alt="" />
+    </p>
+  </Modal>
+</template>
