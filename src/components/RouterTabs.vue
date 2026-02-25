f7d433a39981c3e63f31ec088135529d29335e96
gervinoun <gervinpro@gmail.com>
2026-02-25 13:24:13 +0200

    Update RouterTabs.vue

--- src/components/RouterTabs.vue
+++ src/components/RouterTabs.vue
@@ -1,21 +1,14 @@
 <script setup>
-import { useRoute, useRouter } from 'vue-router'
 
-const router = useRouter()
-const route = useRoute()
-
-console.log(router.getRoutes(), route)
 </script>
 
 <template>
   <div class="tabs is-centered">
     <ul>
       
-        <li class="is-active"><RouterLink to="/">ToDo</RouterLink></li>
-      
-      
-        <li><RouterLink to="/modals">Modals</RouterLink></li>
-      
+        <li v-for="route in $router.getRoutes()" :class="{'is-active': route.path === $route.path}">
+            <RouterLink :to="route.path">{{ route.name }}</RouterLink></li>
+
     </ul>
   </div>
 </template>
