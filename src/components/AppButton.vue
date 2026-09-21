9687d318cd99938ad0da40aee1d8e83891c2196f
gervinoun <gervinoun@gmail.com>
2026-09-21 23:11:45 +0300

    Add button

---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  buttonClass: {
+    type: String,
+    default: "button",
+  },
+});
+
+defineEmits(["click"]);
+</script>
+
+<template>
+  <button
+    :class="buttonClass"
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
